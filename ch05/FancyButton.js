612145512b21252d4bcab930cba07dbd2fce30d4
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2019-02-08 01:00:59 +0900

    5.2 ライフサイクルを利用した振る舞いの定義

--- ch05/FancyButton.js
+++ ch05/FancyButton.js
@@ -3,9 +3,27 @@ class FancyButton extends HTMLElement {
     super();
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.innerHTML = `
-      <style>button { font-size: 10px }</style>
       <button><slot /></button>
     `;
+    this.button = this.shadowRoot.querySelector('button');
+  }
+
+  connectedCallback() {
+    this.addEventListener('click', this.onClick);
+    this.button.addEventListener('click', this.onClickButton);
+  }
+
+  disconnectedCallback() {
+    this.removeEventListener('click', this.onClick);
+    this.button.removeEventListener('click', this.onClickButton);
+  }
+
+  onClick(e) {
+    console.log(e, 'is clicked');
+  }
+
+  onClickButton(e) {
+    console.log(e, 'is clicked');
   }
 }
 
