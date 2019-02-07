c0b3eac8cf64fc562878089215f54ca9f4735ab7
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2019-02-08 00:40:46 +0900

    4.1 ES2015のモジュール構文

---
+++ ch04/app.js
@@ -0,0 +1,4 @@
+import { square, cube } from "./math.js";
+
+console.log(square(3));
+console.log(cube(2));
---
+++ ch04/math.js
@@ -0,0 +1,7 @@
+export function square(n) {
+  return n * n;
+}
+
+export function cube(n) {
+  return n * n * n;
+}
