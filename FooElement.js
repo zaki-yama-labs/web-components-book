c00d0fa181996e3e3755930188d552af8558821c
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2019-02-07 19:35:10 +0900

    2.2 ライフサイクル

--- FooElement.js
+++ FooElement.js
@@ -1,6 +1,29 @@
 class FooElement extends HTMLElement {
+  static get observedAttributes() {
+    return ['foo'];
+  }
+
   constructor() {
     super();
+    console.log('<foo-element> is created');
+  }
+
+  connectedCallback() {
+    console.log('<foo-element> is added to HTML');
+  }
+
+  disconnectedCallback() {
+    console.log('<foo-element> is removed from HTML');
+  }
+
+  attributeChangedCallback(attributeName, oldValue, newValue) {
+    if (attributeName === 'foo') {
+      console.log('Changed from ${oldValue} to ${newValue}');
+    }
+  }
+
+  adoptedCallback() {
+    console.log('<foo-element> is adopted in HTML');
   }
 }
 
